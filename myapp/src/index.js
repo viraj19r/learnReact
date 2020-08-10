a4489dd85582476d747ae233d1b4cddc2f98a3f0
viraj <viraj19r@gmail.com>
2020-08-10 16:37:05 +0530

    create first react component

---
+++ myapp/src/index.js
@@ -0,0 +1,13 @@
+// import the react and reactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+// create a react component
+const App = function(){
+    return <div>Hi there!</div>;
+};
+
+// take the react component and show it on the screen   
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
