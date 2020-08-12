a04c1ad181b79c6e83b631b036c3969bcb6e6924
viraj <viraj19r@gmail.com>
2020-08-12 13:48:47 +0530

    building component with jsx

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,12 +2,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // create a react component
-const App = function(){
-    return <div>Hi there!</div>;
-};
-
-// take the react component and show it on the screen   
+const App = function () {
+    const buttonText = "click me";
+  return <div>
+  <label className="label" htmlFor="name">Enter Name:</label>  
+  <input id="name" type="text" />
+  <button style={{backgroundColor:'green',color: 'white'}}>{buttonText}</button>
+  </div>; // here we will use the htmlFor instead of using the for because in this case there will be an warning form the browser console -->
+  // opening tag for element should be on the same line in which we use the return statement,otherwise undefined will be returned by the return statement
+};// another thing we can do is we can wrap our code inside the parenthesis.
+// take the react component and show it on the screen
 ReactDOM.render(
-    <App />,
-    document.querySelector("#root")
-)
+  <App />,
+  document.querySelector("#root") // this element with id is in the index.html file in public folder
+);
