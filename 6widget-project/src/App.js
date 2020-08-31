e6ebfcea52f12c5c2680323ae083b105b45ff91c
viraj <viraj19r@gmail.com>
2020-08-31 21:25:43 +0530

    create all the widget

--- 6widget-project/src/App.js
+++ 6widget-project/src/App.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+const items = [
+  {
+    title: "what is react ?",
+    content: "React is a frontend javascript framework",
+  },
+  {
+    title: "why use react ?",
+    content: "React is favorite JS library among engineers",
+  },
+  {
+    title: "How do we use react ?",
+    content: "we use react by creating components.",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+];
 export default () => {
-  const items = [
-    {
-      title: "what is react ?",
-      content: "React is a frontend javascript framework",
-    },
-    {
-      title: "why use react ?",
-      content: "React is favorite JS library among engineers",
-    },
-    {
-      title: "How do we use react ?",
-      content: "we use react by creating components.",
-    },
-  ];
+
+
   return (
     <div>
-      <Accordion items={items}/>
+ <Translate /> 
     </div>
   );
 };
