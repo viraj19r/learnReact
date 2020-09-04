5543acbf75e62ee64fe43eb4510067e067486fb0
viraj <viraj19r@gmail.com>
2020-09-04 17:58:58 +0530

    react redux project

--- react-redux project/1songs-project/src/components/SongList.js
+++ react-redux project/1songs-project/src/components/SongList.js
@@ -7,7 +7,7 @@ class SongList extends Component {
     return this.props.songs.map((song) => {
       return (
         <div className="item" key={song.title}>
-          <div className="right floated content">
+          <div className="right floated content">     
             <button onClick={() => this.props.selectSong(song)} className="ui button primary">
               Select
             </button>
